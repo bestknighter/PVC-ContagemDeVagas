b256cfa9ee8fed2d3d4c9cd2f0f81d9d44a8a0ea
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2017-06-14 02:27:30 -0300

    Computa a transformada de hough

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,14 @@ using namespace cv;
 // Termina GLCM
 
 // Começa Hough (Transformada de Hough (rho theta), mesmo alpha)
-
-
-
+#ifndef M_PI
+    #define M_PI 3.141592653589793238L
+#endif
+#define BIN_THRES 0.5 // Threshold da binarizacao
+#define HOUGH_THRES 300 // Acumulador
+#define TAM_DILATA 6 // Pixels
+#define TAM_ERODE 5 // Pixels
+#define TAM_LINHA 1000 // Pixels
 // Termina Hough
 
 
@@ -23,7 +28,13 @@ int main(int argc, char** argv){
         exit(1);
     }
     // Read image
-    Mat img = imread(argv[1]);
+    Mat img = imread(argv[1], IMREAD_COLOR);
+
+    if(!img.data) {
+        printf("Falha ao abrir a imagem.\n");
+        exit(1);
+    }
+
     img.convertTo(img, CV_32F, 1/255.0);
     
     // Calculate gradients gx, gy
@@ -47,9 +58,44 @@ int main(int argc, char** argv){
     // Termina GLCM
 
     // Começa Hough (Transformada de Hough (rho theta), mesmo alpha)
+    {
+        // Binariza
+        Mat binarizado, gray, operado;
+        cvtColor(mag, gray, COLOR_BGR2GRAY);
+        threshold(gray, binarizado, BIN_THRES, 255, THRESH_BINARY);
+        binarizado.convertTo(binarizado, CV_8UC1);
+        imshow("Hough - binarizado", binarizado);
 
+        // Faz fechamento customizado
+        morphologyEx(binarizado, operado, MORPH_DILATE, getStructuringElement( MORPH_ELLIPSE, Size(TAM_DILATA, TAM_DILATA) ));
+        morphologyEx(operado, operado, MORPH_ERODE, getStructuringElement( MORPH_ELLIPSE, Size(TAM_ERODE, TAM_ERODE) ));
+        imshow("Hough - operado", operado);
 
+        { // Acha as linhas por Hough
+            std::vector<Vec2f> lines;
+            HoughLines(operado, lines, 1, M_PI/180., HOUGH_THRES);
+            
+            Mat houghWithLines = gray.clone();
+            cvtColor(operado, houghWithLines, COLOR_GRAY2BGR);
+            for( unsigned int i = 0; i < lines.size(); i++ ) {
+                float rho = lines[i][0];
+                float theta = lines[i][1];
 
+                double a = cos(theta), b = sin(theta);
+                double x0 = a*rho, y0 = b*rho;
+
+                Point pt1(cvRound( x0 + TAM_LINHA*(-b) ), cvRound( y0 + TAM_LINHA*a ));
+                Point pt2(cvRound( x0 - TAM_LINHA*(-b) ), cvRound( y0 - TAM_LINHA*a ));
+                line( houghWithLines, pt1, pt2, Scalar(0,0,255), 1, 8 );
+            }
+
+            imshow("Hough - Linhas", houghWithLines);
+        }
+
+        // { // Acha os segmentos de linhas por Hough
+        // }
+
+    }
     // Termina Hough
 
     waitKey(0);
